bc2a801f41003c0d638abe0ee84e7bc95401f410
Lincoln <117424099+codelincoln@users.noreply.github.com>
2022-11-09 13:28:06 -0800

    Add nav menus

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,9 +11,18 @@ import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 import { provideStorage,getStorage } from '@angular/fire/storage';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { NavComponent } from './shared/nav/nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
@@ -23,7 +32,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
     provideStorage(() => getStorage()),
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+	MatToolbarModule,
+ LayoutModule,
+ MatButtonModule,
+ MatSidenavModule,
+ MatIconModule,
+ MatListModule
   ],
   providers: [
     ScreenTrackingService,UserTrackingService
