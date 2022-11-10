91e8ed87761f48567c00a68313d96ea4d4ae3f7f
Lincoln <117424099+codelincoln@users.noreply.github.com>
2022-11-09 18:40:28 -0800

    Add login feedback

--- src/app/user/user-auth.service.ts
+++ src/app/user/user-auth.service.ts
@@ -1,15 +1,32 @@
 import { Injectable } from '@angular/core';
-import {Auth} from '@angular/fire/auth';
-import {GoogleAuthProvider, signInWithPopup} from '@firebase/auth';
+import { Auth, onAuthStateChanged } from '@angular/fire/auth';
+import { GoogleAuthProvider, signInWithPopup } from '@firebase/auth';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class UserAuthService {
+	uid: string = "";
 
-  constructor(private fbAuth: Auth) { }
+	constructor(private fbAuth: Auth) { 
+		onAuthStateChanged(fbAuth, (user) => {
+			if (user) {
+				this.uid = user.uid;
+			} else {
+				this.uid = "";
+			}
+		});
+	}
 
-  async loginWithGoogle() {
-	  return await signInWithPopup(this.fbAuth, new GoogleAuthProvider());
-  }
+	isLoggedIn(): boolean {
+		return this.uid !== "";
+	}
+
+	getUserID(): string {
+		return this.uid;
+	}
+
+	async loginWithGoogle() {
+		return await signInWithPopup(this.fbAuth, new GoogleAuthProvider());
+	}
 }
